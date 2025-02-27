e3b32b3674492a8e870aa4a87ebc8f8859593ab7
Diego T.I <diraziel@hotmail.com>
2025-02-27 16:02:37 -0300

    Target Sistemas

---
+++ src/01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+int Indice = 13;
+int Soma = 0;
+int K = 0;
+
+while (K <= Indice)
+{
+    K = K + 1;
+    Soma = Soma + K;
+}
+
+Console.WriteLine(Soma);
+
+// valor= 105
+
+
+//observe o trecho de codigo abaixo:
+// int Indice = 13, soma = 0, k = 0;
+//Enquanto K<Indice faça {K=K+1; Soma=Soma+K;}
+//Imprimir(Soma)
+//Ao final do processamento, qual sera o valor da variavel Soma?
+
+//resposta 105;
---
+++ src/03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double[] faturamento = {
+            22174.1664, 24537.6698, 26139.6134, 0, 0, 26742.6612, 0, 42889.2258, 46251.174, 11191.4722,
+            0, 0, 3847.4823, 373.7838, 2659.7563, 48924.2448, 18419.2614, 0, 0, 35240.1826,
+            43829.1667, 18235.6852, 4355.0662, 13327.1025, 0, 0, 25681.8318, 1718.1221, 13220.495, 8414.61
+        };
+
+        var diasComFaturamento = faturamento.Where(f => f > 0).ToArray();
+
+        if (diasComFaturamento.Length == 0)
+        {
+            Console.WriteLine("Nenhum faturamento foi registrado no mês.");
+            return;
+        }
+
+        double menorValor = diasComFaturamento.Min();
+        double maiorValor = diasComFaturamento.Max();
+        double mediaMensal = diasComFaturamento.Average();
+        int diasAcimaDaMedia = faturamento.Count(f => f > mediaMensal);
+
+        Console.WriteLine($"Menor valor de faturamento: {menorValor:C}");
+        Console.WriteLine($"Maior valor de faturamento: {maiorValor:C}");
+        Console.WriteLine($"Número de dias com faturamento acima da média mensal: {diasAcimaDaMedia}");
+    }
+}
